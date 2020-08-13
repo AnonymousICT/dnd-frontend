d8970e4801fd31e61d9cf940a716ad60fb5fea24
AnonymousICT <kuohoward@gmail.com>
2020-08-12 18:14:25 -0700

    Table has been componensized

--- src/components/Equipment/AllEquipment.js
+++ src/components/Equipment/AllEquipment.js
@@ -1,28 +1,8 @@
-import React, {useState, useContext} from 'react'
+import React, { useContext } from 'react'
 import {ResourceContext} from '../../context/ResourceContext'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import TablePagination from '@material-ui/core/TablePagination';
+import RenderTable from '../Table/RenderTable'
 
 export default function AllEquipment() {
-    const [rowsPerPage, setRowsPerPage] = React.useState(20);
-    const [page, setPage] = useState(0);
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-    
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-
-
     const {
         allEquipment, 
         equipmentCategories, 
@@ -33,52 +13,13 @@ export default function AllEquipment() {
 
     const filteredGear = (gearType) => gearType? equipmentCategory : allEquipment ||[]
 
-    // tomorrow's plan is to create a item detail page and then within that page have an option to select a character and add it to that character
-
     return (
         <div className="equipment-container">
             <div className="equipment-filters">
                 {!equipmentCategories ? <h1>Loading...</h1> : equipmentCategories.map(category => <button onClick={(e)=>setEquipmentSelection(e.target.value)} value={category[1]} key={category[0]}>{category[0]}</button>)}
             </div>
             <div className="table-container">
-                <div>
-                <TablePagination
-                        rowsPerPageOptions={[10, 20, 50]}
-                        component="div"
-                        count={filteredGear(equipmentCategory).length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                    />                      
-                    <TableContainer component={Paper}>
-                        <Table className="table" aria-label="simple table" size={'small'}>
-                            <TableHead>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                            </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {filteredGear(equipmentCategory).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                                <TableRow className="tbl-row" key={row.name} onClick={(e)=>setSpecificEquipmentSelection(row.url)}>
-                                    <TableCell align="center" component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <TablePagination
-                        rowsPerPageOptions={[10, 20, 50]}
-                        component="div"
-                        count={filteredGear(equipmentCategory).length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                    />   
-                </div>
+                <RenderTable data={filteredGear(equipmentCategory)} setSpecificData={setSpecificEquipmentSelection} />
             </div>
         </div>
     )
--- src/components/Spells/AllSpells.js
+++ src/components/Spells/AllSpells.js
@@ -3,14 +3,8 @@ import {ResourceContext} from '../../context/ResourceContext'
 import { Context } from '../../context/Context'
 import {fetchAllSpecificSpellData} from '../../api/SpellAPI'
 import bookloading from './bookloading.gif'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import TablePagination from '@material-ui/core/TablePagination';
+import RenderTable from '../Table/RenderTable'
+
 
 export default function AllSpells() {
     const [loading, setLoading] = useState(true)
@@ -18,18 +12,6 @@ export default function AllSpells() {
     const [spellMasterList, setSpellMasterList] = useState([])
     const { selectCharacter, allCharacters } = useContext(Context)
     const { setSpellSelection } = useContext(ResourceContext)
-    const [rowsPerPage, setRowsPerPage] = React.useState(20);
-    const [page, setPage] = useState(0);
-
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-    
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
 
     useEffect(() => {
         setLoading(true);
@@ -59,50 +41,10 @@ export default function AllSpells() {
     }
     ,[allCharacters, selectCharacter, spellMasterList])
 
-
     return (
         <div className='all-spells-container'>
             <div className="table-container">
-                {loading ? <img src={bookloading} alt="Loading..." /> : 
-                <div>
-                    <TablePagination
-                        rowsPerPageOptions={[10, 20, 50]}
-                        component="div"
-                        count={allSpellModels.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                    />                      
-                    <TableContainer component={Paper}>
-                        <Table className="table" aria-label="simple table" size={'small'}>
-                            <TableHead>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                            </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {allSpellModels.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                                <TableRow className="tbl-row" key={row.name} onClick={(e)=>setSpellSelection(row.url)}>
-                                    <TableCell align="center" component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <TablePagination
-                        rowsPerPageOptions={[10, 20, 50]}
-                        component="div"
-                        count={allSpellModels.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                    />   
-                </div>             
-                }
+                {loading ? <img src={bookloading} alt="Loading..." /> : <RenderTable data={allSpellModels} setSpecificData={setSpellSelection} />}
             </div>
         </div>
     )
