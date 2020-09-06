2b87bb7d35bc716ff5425e6ed9d388ec45730d61
AnonymousICT <kuohoward@gmail.com>
2020-09-06 06:16:32 -0700

    fixed specific spell container bug

--- src/App.js
+++ src/App.js
@@ -9,74 +9,78 @@ import CreateCharacter from "./components/Characters/CreateCharacter/CreateChara
 import EditCharacter from "./components/Characters/EditCharacter/EditCharacter";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 import "./App.scss";
 import User from "./components/User/User";
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#eaa260',
+    palette: {
+        primary: {
+            main: "#eaa260",
+        },
+        secondary: {
+            main: "#31a065",
+        },
     },
-    secondary: {
-      main: '#31a065',
-    },
-  },
 });
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-
-    <div className="App">
-      <header>
-        <h1>D&D CG</h1>
-        <h3>
-          <span>5th edition</span> character generator
-        </h3>
-        <Navbar />
-      </header>
-      <main>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/spells">
-            <Spells />
-          </Route>
-          <Route path="/equipment">
-            <Equipment />
-          </Route>
-          <Route path="/characters">
-            <Characters />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/createCharacter">
-            <CreateCharacter />
-          </Route>
-          <Route path="/character/edit">
-            <EditCharacter />
-          </Route>
-          <Route path="/user">
-            <User />
-          </Route>
-        </Switch>
-      </main>
-      <footer>
-        <p>
-          Art assets are courtesy of{" "}
-          <a href="https://www.melissapalacios.com/" target="blank">
-            Melissa Palacios
-          </a>
-        </p>
-      </footer>
-    </div>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <div className="Hero">
+                    <header>
+                        <h1>D&D CG</h1>
+                        <h3>
+                            <span>5th edition</span> character generator
+                        </h3>
+                        <Navbar />
+                    </header>
+                    <main>
+                        <Switch>
+                            <Route exact path="/">
+                                <Home />
+                            </Route>
+                            <Route path="/spells">
+                                <Spells />
+                            </Route>
+                            <Route path="/equipment">
+                                <Equipment />
+                            </Route>
+                            <Route path="/characters">
+                                <Characters />
+                            </Route>
+                            <Route path="/login">
+                                <Login />
+                            </Route>
+                            <Route path="/register">
+                                <Register />
+                            </Route>
+                            <Route path="/createCharacter">
+                                <CreateCharacter />
+                            </Route>
+                            <Route path="/character/edit">
+                                <EditCharacter />
+                            </Route>
+                            <Route path="/user">
+                                <User />
+                            </Route>
+                        </Switch>
+                    </main>
+                    <footer>
+                        <p>
+                            Art assets are courtesy of{" "}
+                            <a
+                                href="https://www.melissapalacios.com/"
+                                target="blank"
+                            >
+                                Melissa Palacios
+                            </a>
+                        </p>
+                    </footer>
+                </div>
+            </div>
+        </ThemeProvider>
+    );
 }
